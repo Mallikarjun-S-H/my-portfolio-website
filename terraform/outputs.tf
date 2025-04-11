5f0791b327fe198597de8d7011c3d3b66d2c2f7a
Mallikarjun-S-H <143923793+Mallikarjun-S-H@users.noreply.github.com>
2025-04-11 22:12:15 +0530

    o

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
+output "cluster_ca_certificate" {
+  value = module.eks.cluster_certificate_authority_data
 }
