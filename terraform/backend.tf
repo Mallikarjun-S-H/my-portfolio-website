db16a88d3c67664a5281d550ab049e5e6bb176e5
Mallikarjun-S-H <143923793+Mallikarjun-S-H@users.noreply.github.com>
2025-04-12 23:33:54 +0530

    dw

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "statelock121"   # must be correct
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"      # optional
-  }
-}
--- terraform/ecr.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ecr_repository" "app" {
-  name = "demo/portfolio"
-
-
-}
